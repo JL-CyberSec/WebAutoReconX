0961c224852345ddf2a543636c03f5edb5434b04
Leandro Panesso Atehortúa <leandro@queo.com.co>
2024-08-16 16:10:05 -0500

    feat: Setup

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,15 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [
+                ...refreshPaths,
+                'app/Filament/**',
+                'app/Forms/Components/**',
+                'app/Livewire/**',
+                'app/Infolists/Components/**',
+                'app/Providers/Filament/**',
+                'app/Tables/Columns/**',
+            ],
         }),
     ],
 });
