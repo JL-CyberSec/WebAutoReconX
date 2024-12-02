15145b338513289c1ab922e3bef7137bdff31446
Jhon Alejandro Piraquive Ramirez <jpiraquive@heinsohn.com.co>
2024-12-02 11:44:54 -0500

    Add unit test

---
+++ tests/Feature/ScanJobTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ScanJobTest extends TestCase
+{
+    public function test_scan_job_system_information_works(): void
+    {
+        $this->assertEquals(200, 200);
+    }
+
+    public function test_scan_job_interfaces_works(): void
+    {
+        $this->assertEquals(200, 200);
+    }
+
+    public function test_scan_job_firewall_works(): void
+    {
+        $this->assertEquals(200, 200);
+    }
+
+    public function test_scan_job_hosts_works(): void
+    {
+        $this->assertEquals(200, 200);
+    }
+}
